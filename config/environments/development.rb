f99a4e52aa42fd458e9851aa389db2a004fadec6
Jeffrey Ben <jefficyben8@gmail.com>
2020-01-16 17:38:39 -0500

    Finished Chapter 4

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+
+  Rails.application.config.x.shopify_metrics_disable_puma_plugin = true
+  
   # Do not eager load code on boot.
   config.eager_load = false
 
